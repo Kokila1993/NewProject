fe6e40b8b37f711a0dcbf176dbbe8c54a6a78639
Kokila1993 <koki.nataraj@gmail.com>
2023-08-16 18:10:08 +0530

    Employee Age Added

--- src/org/login/EmployeeDetails.java
+++ src/org/login/EmployeeDetails.java
@@ -5,6 +5,8 @@ public class EmployeeDetails {
 	private void EmployeeInfo() {
 
 		System.out.println("Employee Salary :50000");
+		System.out.println("Employee age : 29");
+		
 	}
 	
 	public static void main(String[] args) {
