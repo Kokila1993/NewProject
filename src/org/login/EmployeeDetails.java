c30c1a98212b887c0ea65a4d4d541f0e8804bc99
Kokila1993 <koki.nataraj@gmail.com>
2023-08-16 17:44:40 +0530

    BranchChange Learn

---
+++ src/org/login/EmployeeDetails.java
@@ -0,0 +1,16 @@
+package org.login;
+
+public class EmployeeDetails {
+	
+	private void EmployeeInfo() {
+
+		System.out.println("Employee Salary :50000");
+	}
+	
+	public static void main(String[] args) {
+		
+		EmployeeDetails e = new EmployeeDetails();
+		e.EmployeeInfo();
+	}
+
+}
