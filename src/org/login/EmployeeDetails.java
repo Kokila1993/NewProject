bd11b9d406b29fc250341a63f94d116e5826248b
Kokila1993 <koki.nataraj@gmail.com>
2023-08-17 22:28:39 +0530

    Dev Team Has made a change

--- src/org/login/EmployeeDetails.java
+++ src/org/login/EmployeeDetails.java
@@ -5,6 +5,8 @@ public class EmployeeDetails {
 	private void EmployeeInfo() {
 
 		System.out.println("Employee Salary :50000");
+		System.out.println("Employee Age : 29");
+		System.out.println("Employee Mobile : 1234567890");
 	}
 	
 	public static void main(String[] args) {
